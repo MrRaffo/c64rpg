8bce815fdbaecd169f5f28784343aef7075f8068
Paul Rafferty <prafferty@outlook.com>
2019-10-09 13:40:40 +0100

    Display charset

--- rpg.asm
+++ rpg.asm
@@ -1,6 +1,7 @@
 !to "rpg.prg",cbm
 
-;ver 1:loads charset and sprite data, initializes VIC, tests first sprite definitions
+;ver 1: loads charset and sprite data, initializes VIC, tests first sprite definitions
+;ver 2: added simple joystick controls for player sprite and tests charset
 
 ;=============================================================
 ;CONSTANTS
@@ -79,13 +80,21 @@ ZEROPAGE_POINTER_2              = $19
 ZEROPAGE_POINTER_3              = $1b
 ZEROPAGE_POINTER_4              = $1d
 
-NUMBER_OF_SPRITES               = $08
+NUMBER_OF_SPRITES               = $05
 SPRITE_BASE                     = $40               ;64 in decimal
 SPRITE_END                      = SPRITE_BASE + NUMBER_OF_SPRITES
 SPRITE_POINTER_BASE             = SCREEN_CHAR_BUFFER + $3f8     ;1016 bytes after screen buffer
 SPRITE_DATA_BUFFER              = $d000             ;put sprite definitions here
 
+;LEVEL CONSTANTS
+LEVEL_TILE_WIDTH                = 20                ;tiles are 2x2
+LEVEL_TILE_HEIGHT               = 11
 
+TILE_SIDE                       = 16                ;size of a tile in pixels (length of a side)
+TILE_HALF_SIDE                  = 8
+
+SPRITE_SIZE                     = 16
+SPRITE_HALF_SIZE                = 8
 
 ;=============================================================
 ;   INITIALIZATION
@@ -160,7 +169,57 @@ SPRITE_DATA_BUFFER              = $d000             ;put sprite definitions here
 
         jsr ClearScreen             ;clear the screen
 
-        jsr TEST_SpriteData
+;=================================
+;INITIALIZE SPRITE FOR TESTING
+;=================================
+
+
+        lda #$40                    ;64 decimal
+        sta SPRITE_X_POS
+        sta SPRITE_Y_POS            
+
+        sta SPRITE_POINTER_BASE
+
+        lda #$01
+        sta VIC_SPRITE_ENABLE_REGISTER
+        sta VIC_SPRITE_MULTICOLOR_REGISTER
+
+        lda #ORANGE
+        sta VIC_SPRITE_COLOR_1
+
+        lda #YELLOW
+        sta VIC_SPRITE_COLOR_2
+
+        lda #RED
+        sta VIC_SPRITE_COLOR_BASE
+
+;==================================
+;INITIALIZE TILE DATA FOR TESTING
+;==================================
+
+        lda VIC_CONTROL_REGISTER_2
+        ora #%00010000                  ;text multicolor mode
+        sta VIC_CONTROL_REGISTER_2
+
+        lda #BROWN
+        sta VIC_BGCOLOR_1
+
+        lda #ORANGE
+        sta VIC_BGCOLOR_2
+
+        lda #$0a
+        tay
+
+        ldx #$00
+
+TestChar
+        txa
+        sta SCREEN_CHAR_BUFFER,x
+        tya
+        sta VIC_COLOR_RAM,x
+        inx
+        bne TestChar
+        
 
 ;==================================================================
 ;       MAIN LOOP
@@ -168,6 +227,10 @@ SPRITE_DATA_BUFFER              = $d000             ;put sprite definitions here
 !zone GameLoop
 GameLoop
         inc VIC_BORDER_COLOR           ;flash the border
+
+        jsr ReadJoystick
+
+        jsr UpdateSpritePositions      ;moves sprite software stored positions to hardware position registers, deals with extended X bit
 
         jsr WaitFrame
 
@@ -304,92 +367,197 @@ WaitFrame
         rts
 
 
+;==================================================================
+;       CONTROL AND MOVEMENT FUNCTIONS
+;==================================================================
+
+;=========================
+;ReadJoystick
+;get js status and branch
+;=========================
+
+!zone ReadJoystick
+ReadJoystick
+        lda #$01
+        bit JOYSTICK_2
+        bne .UpNotPressed
+        jsr PlayerMoveUp
+
+.UpNotPressed
+        lda #$02
+        bit JOYSTICK_2
+        bne .DownNotPressed
+        jsr PlayerMoveDown
+
+.DownNotPressed
+        lda #$04
+        bit JOYSTICK_2
+        bne .LeftNotPressed
+        jsr PlayerMoveLeft
+
+.LeftNotPressed
+        lda #$08
+        bit JOYSTICK_2
+        bne .RightNotPressed
+        jsr PlayerMoveRight
+
+.RightNotPressed
+        lda #$10
+        bit JOYSTICK_2
+        bne .ButtonNotPressed
+        jsr PlayerButton
+
+.ButtonNotPressed
+        rts
+
+;===============================
+;PlayerMoveUp
+;will include code to check if move is possible later
+;===============================
+!zone PlayerMoveUp
+PlayerMoveUp
+        ldx #$00
+        jsr MoveSpriteUp
+        rts
+
+!zone PlayerMoveDown
+PlayerMoveDown
+        ldx #$00
+        jsr MoveSpriteDown
+        rts
+
+!zone PlayerMoveLeft
+PlayerMoveLeft
+        ldx #$00
+        jsr MoveSpriteLeft
+        rts
+
+!zone PlayerMoveRight
+PlayerMoveRight
+        ldx #$00
+        jsr MoveSpriteRight
+        rts
+
+!zone PlayerButton
+PlayerButton
+        ldx #$00
+        jsr PlayerAction
+        rts
+
+;=====================================
+;MoveSpriteUp
+;expects x as sprite index
+;=====================================
+
+!zone MoveSpriteUp
+MoveSpriteUp
+        dec SPRITE_Y_POS,x
+        rts
+
+MoveSpriteDown
+        inc SPRITE_Y_POS,x
+        rts
+
+MoveSpriteLeft
+        dec SPRITE_X_POS,x
+        rts
+
+MoveSpriteRight
+        inc SPRITE_X_POS,x
+        rts
+
+PlayerAction
+        inc VIC_SPRITE_COLOR_BASE,x
+        rts
+
+;====================================
+;UpdateSpritePositions
+;moves sprite pos from ram to vic reg
+;====================================
+
+!zone UpdateSpritePositions
+UpdateSpritePositions
+        ldx #$00            ;index
+.UpdateLoop
+
+        txa
+        asl                 ;sprite positions for x and y coords are interlaced, this gets the right index for the hw coord registers
+        tay                 
+
+        lda SPRITE_Y_POS,x
+        sta VIC_SPRITE_Y_COORD,y        ;Y coord does not need to be modified
+
+        lda SPRITE_X_POS,x
+        asl                 ;effectively doubles position, high bit now in carry
+        sta VIC_SPRITE_X_COORD,y            
+        ror VIC_SPRITE_X_EXTEND
+        inx
+        cpx #$08
+        bne .UpdateLoop
+
+        rts
+        
+;==================================================================
+;       LEVEL FUNCTIONS
+;==================================================================
+!zone DrawTile
+
+;==================================================================
+;       MATHS FUNCTIONS
+;==================================================================
+
+;=====================================
+;Multiply
+;takes operands in MULTI1 and MULTI2
+;preferably put lower number in MULTI1
+;result stored in MULTIL and MULTIH
+;=====================================
+
+!zone Multiply
+Multiply
+        ldx #$00
+        stx MULTIL
+        stx MULTIH
+        txa             ;zero result and accumulator in prep
+
+        ldx MULTI2
+        beq .MultiplyDone
+        ldx MULTI1
+        beq .MultiplyDone       ;if either operand is 0, return immediately
+
+        clc                     ;otherwise, clear the carry before beginning
+
+.MultiplyLoop
+        adc MULTI2              ;accumulator has been zeroed, continue adding MULTI2 MULTI1 times
+        bcc .NoOverflow
+
+        inc MULTIH              ;increment the high byte if accumulator overflows
+
+.NoOverflow
+        dex
+        bne .MultiplyLoop       ;if x is zero, we've finished
+
+.MultiplyDone
+        sta MULTIL              ;accumulator holds low byte of the result, high byte is set during routine
+        rts
 
 ;==================================================================
 ;       TEST FUNCTIONS
 ;==================================================================
-!zone TEST_SpriteData
-TEST_SpriteData
-  
-        ldx #$ff
-        stx VIC_SPRITE_ENABLE_REGISTER
-        stx VIC_SPRITE_MULTICOLOR_REGISTER
-
-        stx $d017       ;sprites double height
-        stx $d01d       ;sprites double width
-
-        ldx #$64        ;100 decimal, all sprites shown at this Y-coord
-        stx VIC_SPRITE_Y_COORD
-        stx VIC_SPRITE_Y_COORD + 2
-        stx VIC_SPRITE_Y_COORD + 4
-        stx VIC_SPRITE_Y_COORD + 6
-        stx VIC_SPRITE_Y_COORD + 8
-        stx VIC_SPRITE_Y_COORD + 10
-        stx VIC_SPRITE_Y_COORD + 12
-        stx VIC_SPRITE_Y_COORD + 14
-
-        ldx #$20
-        stx VIC_SPRITE_X_COORD
-        ldx #$40
-        stx VIC_SPRITE_X_COORD + 2
-        ldx #$60
-        stx VIC_SPRITE_X_COORD + 4
-        ldx #$80
-        stx VIC_SPRITE_X_COORD + 6
-        ldx #$a0
-        stx VIC_SPRITE_X_COORD + 8
-        ldx #$c0
-        stx VIC_SPRITE_X_COORD + 10
-        ldx #$e0
-        stx VIC_SPRITE_X_COORD + 12
-        ldx #$ff
-        stx VIC_SPRITE_X_COORD + 14
-
-        ldx #$02            ;RED
-        stx VIC_SPRITE_COLOR_BASE
-        inx                 ;CYAN
-        stx VIC_SPRITE_COLOR_BASE + 1
-        inx                 ;VIOLET
-        stx VIC_SPRITE_COLOR_BASE + 2
-        inx                 ;GREEN
-        stx VIC_SPRITE_COLOR_BASE + 3
-        inx                 ;BLUE
-        stx VIC_SPRITE_COLOR_BASE + 4
-        ldx #$02            ;RED
-        stx VIC_SPRITE_COLOR_BASE + 5
-        inx                 ;CYAN
-        stx VIC_SPRITE_COLOR_BASE + 6
-        inx                 ;BROWN
-        stx VIC_SPRITE_COLOR_BASE + 7
-
-        ldx #ORANGE
-        stx VIC_SPRITE_COLOR_1
-        ldx #YELLOW
-        stx VIC_SPRITE_COLOR_2
-
-  
-        ldx #SPRITE_BASE
-        ldy #$00
-
-.TestLoop
-        txa
-        sta SPRITE_POINTER_BASE,y
-        inx
-        iny
-        cpy #$08        ;number of sprites so far
-        bne .TestLoop
-
-.TestSpriteDone
-        rts
-        
 
 
 ;==================================================================
 ;       DATA
 ;==================================================================
 
+SPRITE_X_POS
+        !byte $00, $00, $00, $00, $00, $00, $00, $00
+
+SPRITE_Y_POS
+        !byte $00, $00, $00, $00, $00, $00, $00, $00
+
 CHARSET_DATA
-        !binary "multichar.chr"
+        !binary "graphics/rpgset1.chr"
 
 SPRITE_DATA
-        !binary "graphics/sprites.bin"
+        !binary "graphics/sprites.spr"
